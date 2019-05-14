ba540ada1f6788e22852a062e4e6357387195a4a
baldapps <playappassistance@gmail.com>
2019-05-14 20:01:34 +0200

    Fix false positive

--- Artemis/src/com/baldapps/artemis/checkers/ClassMembersConstChecker.java
+++ Artemis/src/com/baldapps/artemis/checkers/ClassMembersConstChecker.java
@@ -274,7 +274,8 @@ public class ClassMembersConstChecker extends AbstractIndexAstChecker {
 		}
 
 		private boolean isWrittenToNonMutable(IASTName name) {
-			if ((CPPVariableReadWriteFlags.getReadWriteFlags(name) & PDOMName.WRITE_ACCESS) != 0) {
+			if ((CPPVariableReadWriteFlags.getReadWriteFlags(name) & PDOMName.WRITE_ACCESS) != 0
+					&& (CPPVariableReadWriteFlags.getReadWriteFlags(name) & PDOMName.READ_ACCESS) == 0) {
 				IBinding binding = name.resolveBinding();
 				if (binding instanceof ICPPField) {
 					return !((ICPPField) binding).isMutable();
