618d7fcc50f70a95b3804f8b9f31f411be1239cb
baldapps <playappassistance@gmail.com>
2020-06-21 08:16:34 +0200

    Added checker for ctor template priority

---
+++ Artemis/src/com/baldapps/artemis/checkers/CtorTemplateChecker.java
@@ -0,0 +1,105 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Marco Stornelli
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ *******************************************************************************/
+package com.baldapps.artemis.checkers;
+
+import org.eclipse.cdt.codan.core.cxx.model.AbstractIndexAstChecker;
+import org.eclipse.cdt.codan.core.model.IProblemLocation;
+import org.eclipse.cdt.codan.core.model.IProblemLocationFactory;
+import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTDeclSpecifier;
+import org.eclipse.cdt.core.dom.ast.IASTFileLocation;
+import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.dom.ast.IBinding;
+import org.eclipse.cdt.core.dom.ast.IType;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTCompositeTypeSpecifier;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPClassType;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPConstructor;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPFunctionTemplate;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPReferenceType;
+import org.eclipse.cdt.core.dom.ast.cpp.SemanticQueries;
+import org.eclipse.cdt.core.index.IIndex;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.semantics.SemanticUtil;
+import org.eclipse.core.runtime.CoreException;
+
+import com.baldapps.artemis.utils.IndexUtils;
+
+@SuppressWarnings("restriction")
+public class CtorTemplateChecker extends AbstractIndexAstChecker {
+	public static final String CTOR_TEMPLATE_ID = "com.baldapps.artemis.checkers.CtorTemplateProblem"; //$NON-NLS-1$
+
+	private IIndex index;
+
+	@Override
+	public void processAst(IASTTranslationUnit ast) {
+		index = ast.getIndex();
+		ast.accept(new NodeVisitor());
+	}
+
+	private void reportProblem(String problemId, IBinding binding, IASTNode root, Object... args)
+			throws InterruptedException, CoreException {
+		IProblemLocation location = getProblemLocation(binding);
+		if (location == null)
+			reportProblem(problemId, root, args);
+		else
+			reportProblem(problemId, location, args);
+	}
+
+	private IProblemLocation getProblemLocation(IBinding binding) throws InterruptedException, CoreException {
+		IASTFileLocation astLocation = IndexUtils.getLocation(index, binding);
+		if (astLocation == null)
+			return null;
+		int line = astLocation.getStartingLineNumber();
+		IProblemLocationFactory locFactory = getRuntime().getProblemLocationFactory();
+		int start = astLocation.getNodeOffset();
+		int end = start + astLocation.getNodeLength();
+		return locFactory.createProblemLocation(getFile(), start, end, line);
+	}
+
+	private class NodeVisitor extends ASTVisitor {
+
+		public NodeVisitor() {
+			shouldVisitDeclSpecifiers = true;
+		}
+
+		@Override
+		public int visit(IASTDeclSpecifier element) {
+			try {
+				if (element instanceof ICPPASTCompositeTypeSpecifier) {
+					ICPPASTCompositeTypeSpecifier clazz = (ICPPASTCompositeTypeSpecifier) element;
+					if (clazz.getKey() == ICPPASTCompositeTypeSpecifier.k_class) {
+						IBinding binding = clazz.getName().resolveBinding();
+						if (binding instanceof ICPPClassType) {
+							ICPPClassType classType = (ICPPClassType) binding;
+							ICPPConstructor[] ctors = classType.getConstructors();
+							for (ICPPConstructor m : ctors) {
+								if (m instanceof ICPPFunctionTemplate && !m.isImplicit()
+										&& m.getParameters().length == 1 && !SemanticQueries.isCopyOrMoveConstructor(m)
+										&& ((ICPPFunctionTemplate) m).getTemplateParameters().length == 1) {
+									IType t = m.getParameters()[0].getType();
+									IType refType = SemanticUtil.getNestedType(t, SemanticUtil.TDEF);
+									if (refType instanceof ICPPReferenceType
+											&& ((ICPPReferenceType) refType).isRValueReference()) {
+										reportProblem(CTOR_TEMPLATE_ID, m, element);
+									}
+								}
+							}
+						}
+					}
+				}
+			} catch (InterruptedException | CoreException e) {
+				ArtemisCoreActivator.log(e);
+			}
+			return PROCESS_CONTINUE;
+		}
+	}
+}
---
+++ Artemis/src/com/baldapps/artemis/tests/CtorTemplateTest.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Marco Stornelli
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package com.baldapps.artemis.tests;
+
+import com.baldapps.artemis.checkers.CtorTemplateChecker;
+
+/**
+ * Test for {@link CtorTemplateChecker} class
+ */
+public class CtorTemplateTest extends ArtemisCheckerTestCase {
+
+	public static final String ERR_ID = CtorTemplateChecker.CTOR_TEMPLATE_ID;
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		enableProblems(ERR_ID);
+	}
+
+	@Override
+	public boolean isCpp() {
+		return true;
+	}
+
+	@Override
+	public boolean isHeader() {
+		return true;
+	}
+
+	//class A {
+	//public:
+	//	A(int a);
+	//};
+	public void testCtorSinglePar() throws Exception {
+		loadCodeAndRun(getAboveComment());
+		checkNoErrors();
+	}
+
+	//template<class T>
+	//class A {
+	//public:
+	//	A(T&& t) {}
+	//};
+	public void testCtorClassTemplateRValue() throws Exception {
+		loadCodeAndRun(getAboveComment());
+		checkNoErrors();
+	}
+
+	//class A {
+	//public:
+	//	template<class T>
+	//	A(T&& t) {}
+	//};
+	public void testCtorTemplateRValue() throws Exception {
+		loadCodeAndRun(getAboveComment());
+		checkErrorLine(1, ERR_ID);
+	}
+
+	//class A {
+	//public:
+	//	template<class T>
+	//	A(T& t) {}
+	//};
+	public void testCtorTemplateLValue() throws Exception {
+		loadCodeAndRun(getAboveComment());
+		checkNoErrors();
+	}
+}
